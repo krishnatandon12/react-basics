9e5ef2a217760d09cb96cec9ad0a64c880bf0bf4
krishnatandon12 <105916399+krishnatandon12@users.noreply.github.com>
2023-01-02 20:12:37 +0530

    Functions returning strings

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  const someText = 'I am some interpolated text';
-  const someClassName = 'interpolated-class-name';
-  const someMoreText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-  mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-  voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-  fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-  accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-  molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-  officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-  nesciunt ipsum debitis quas aliquid.`;
-
+  const getText = (name) => `My name is ${name}`;
   return (
     <div>
-      <h1>Interpolation in React</h1>
-      <h2 className={someClassName}>{someText}</h2>
-      <p>{someMoreText}</p>
+      <h1 style={{ textDecoration: 'double underline' }}>
+        Functions returning String values
+      </h1>
+      <p>
+        <span style={{ fontSize: '24px', fontWeight: '900', display: 'block' }}>
+          {getText('Krishna')}
+        </span>
+      </p>
     </div>
   );
 }
