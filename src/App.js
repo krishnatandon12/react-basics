d161253832023ad54f3a68a76254581b10b1bf88
krishnatandon12 <105916399+krishnatandon12@users.noreply.github.com>
2023-01-02 16:35:17 +0530

    String Interpolation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './style.css';
 export default function App() {
   const someText = 'I am some interpolated text';
   const someClassName = 'interpolated-class-name';
+  const someMoreText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+  mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+  voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+  fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+  accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+  molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+  officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+  nesciunt ipsum debitis quas aliquid.`;
 
   return (
     <div>
       <h1>Interpolation in React</h1>
-      <p className={someClassName}>{someText}</p>
+      <h2 className={someClassName}>{someText}</h2>
+      <p>{someMoreText}</p>
     </div>
   );
 }
