881331b85651c18893afdefc35b0f29bcd80772c
krishnatandon12 <105916399+krishnatandon12@users.noreply.github.com>
2023-01-02 20:14:40 +0530

    Functions returning numeric values

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  const getText = (name) => `My name is ${name}`;
+  const calculate = (x, y) => x * y;
   return (
     <div>
       <h1 style={{ textDecoration: 'double underline' }}>
-        Functions returning String values
+        Functions returning Numeric values
       </h1>
       <p>
         <span style={{ fontSize: '24px', fontWeight: '900', display: 'block' }}>
-          {getText('Krishna')}
+          Result = {calculate(12, 8)}
         </span>
       </p>
     </div>
