d7dc8252ed367b586c7b1aa7bc4a36b2ac465a4b
krishnatandon12 <105916399+krishnatandon12@users.noreply.github.com>
2023-02-28 16:58:30 +0530

    Functions returning Objects, Views

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import Heading from './Heading';
 import './style.css';
 
 export default function App() {
   const calculate = (x, y) => x * y;
+
+  const getSetObject = (name, age = 10) => ({ type: 'SET_NAME', name, age });
+
+  const getParagraph = () => <p>Krishna</p>;
+
   return (
     <div>
-      <h1 style={{ textDecoration: 'double underline' }}>
-        Functions returning Numeric values
-      </h1>
+      <Heading title="Functions returning Numeric values" />
+      <p>
+        <span style={{ fontSize: '24px', fontWeight: '700', display: 'block' }}>
+          Result = {calculate(12, 8)}
+        </span>
+      </p>
+
+      <Heading title="Functions returning Objects" />
       <p>
         <span style={{ fontSize: '24px', fontWeight: '900', display: 'block' }}>
-          Result = {calculate(12, 8)}
+          Result = {JSON.stringify(getSetObject('Krishna', 12))}
+        </span>
+      </p>
+
+      <Heading title="Functions returning Views" />
+      <p>
+        <span style={{ fontSize: '24px', fontWeight: '900', display: 'block' }}>
+          Result = {getParagraph()}
         </span>
       </p>
     </div>
---
+++ src/Heading.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Heading = ({ title }) => {
+  return <h1 style={{ textDecoration: 'dashed underline' }}>{title}</h1>;
+};
+
+export default Heading;
